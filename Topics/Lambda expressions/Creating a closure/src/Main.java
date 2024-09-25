d736b0afe7da61ed69d3f2e62a9af86e898cf3e6
zoranjuras <60330306+zoranjuras@users.noreply.github.com>
2024-09-25 14:10:53 +0200

    Delete Topics directory

--- Topics/Lambda expressions/Creating a closure/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.function.UnaryOperator;
-
-class PrefixSuffixOperator {
-
-    public static final String PREFIX = "__pref__";
-    public static final String SUFFIX = "__suff__";
-
-    public static UnaryOperator<String> operator = s -> PREFIX + s.trim() + SUFFIX;
-}
--- Topics/Lambda expressions/Max of two integers/src/Main.java
+++
@@ -1,6 +0,0 @@
-import java.util.function.IntBinaryOperator;
-
-class Operator {
-
-    public static IntBinaryOperator binaryOperator = (x, y) -> x > y? x : y;
-}
